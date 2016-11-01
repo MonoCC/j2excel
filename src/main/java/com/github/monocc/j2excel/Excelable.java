8605dd4c44b7311e29e8081bc79c38c73891ce08
monoCc <chongdi.yang@outlook.com>
2016-11-01 14:44:50 +0800

    add guava and rename projectname

---
+++ src/main/java/com/github/monocc/j2excel/Excelable.java
@@ -0,0 +1,18 @@
+package com.github.monocc.j2excel;
+
+import java.util.List;
+
+/**
+ * Excel有多个sheet组成
+ * @author monocc
+ */
+public class Excelable {
+	
+	
+	private List<String> sheetNames;
+	
+	public void sheetNames(String... name) {
+		
+	}
+
+}
